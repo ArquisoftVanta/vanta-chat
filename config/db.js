58e8d7440a4f8aa40a4ec87d1488cf11e38ee19b
Mauricio Meza <mmezab@unal.edu.co>
2021-04-01 23:43:09 -0500

    configures mongodbatlas

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const dbURL = process.env.DB_URL || "mongodb+srv://userapp:Ba8xUt88LgiCkdh6@cluster0.xoq7j.mongodb.net/Chat?retryWrites=true&w=majority"
 
 const connect = mongoose.connect(dbURL,  {useNewUrlParser:true, useUnifiedTopology:true}, () => { 
-    console.log("mongodb connected at: " + dbURL)
+    console.log("mongodb connected at: MongoDBAtlas")
 })
 
 module.exports = connect
