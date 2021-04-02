76b33f54ca1ff2a25373667a16cc3446a2a0e335
Mauricio Meza <mmezab@unal.edu.co>
2021-04-01 23:19:57 -0500

    changes porque soy bien bruto

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const dbURL = process.env.DB_URL || "mongodb+srv://<user>:<user>@cluster0.xoq7j.mongodb.net/Chat?retryWrites=true&w=majority"
+const dbURL = process.env.DB_URL || "mongodb+srv://user:user@cluster0.xoq7j.mongodb.net/Chat?retryWrites=true&w=majority"
 
 const connect = mongoose.connect(dbURL,  {useNewUrlParser:true, useUnifiedTopology:true}, () => { 
     console.log("mongodb connected at: " + dbURL)
