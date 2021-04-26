d30a7165e5efa37c4171a729814115d12bc09546
Gonzalo Diaz <egonzalodm@gmail.com>
2021-04-25 21:44:40 -0500

    cors habilitado

--- app.js
+++ app.js
@@ -1,38 +1,36 @@
-const http = require('http');
+const http = require("http");
 const express = require("express");
-const io = require('socket.io');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const io = require("socket.io");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 //Connect DB
-dotenv.config()
-const DB  = require("./config/db");
-const socketConnection  = require("./config/sockets");
-const socketFunctionality = require("./config/socketFuncs.js")
+dotenv.config();
+const DB = require("./config/db");
+const socketConnection = require("./config/sockets");
+const socketFunctionality = require("./config/socketFuncs.js");
 
 const port = process.env.PORT || 8080;
 
 //Serving Express Aplication
 app = express();
-server = http.Server(app);
 app.use(express.json());
 app.use(cors());
+server = http.Server(app);
 
 //Connecting Socket Server
-socket = io(server);
+socket = io(server, { cors: { origin: "*" } });
 socket.use(socketConnection);
-socket.on("connection", socketFunctionality)
-
+socket.on("connection", socketFunctionality);
 
 //Gets API Routes from conversation
 const convRouter = require("./routes/convRoutes");
 app.use("/conv", convRouter);
 
-
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
 server.listen(port, () => {
-    console.log("Server connected on" + port)
-})
+  console.log("Server connected on" + port);
+});
